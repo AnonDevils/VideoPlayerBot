afe710eea4ef797b99ddbfceb3f9ee343f637dd3
ANON DEVIL <106466505+AnonDevils@users.noreply.github.com>
2022-06-29 23:59:20 +0700

    Update translations.py

--- translations.py
+++ translations.py
@@ -2,7 +2,7 @@ from config import ASSISTANT_NAME
 from helpers.bot_utils import BOT_NAME, USERNAME
 
 
-START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By @ImSafone!** 👑"
+START_TEXT = f"👋🏻 **𝙃𝙖𝙡𝙤**, \n\n𝙄𝙣𝙞 𝙖𝙙𝙖𝙡𝙖𝙝 **{BOT_NAME}** \n𝙎𝙖𝙮𝙖 𝘽𝙤𝙡𝙚𝙝 𝙎𝙩𝙧𝙚𝙖𝙢 𝙇𝙞𝙫𝙚𝙨, 𝙍𝙖𝙙𝙞𝙤, 𝙑𝙞𝙙𝙚𝙤 𝙔𝙤𝙪𝙏𝙪𝙗𝙚 & 𝙁𝙖𝙞𝙡 𝘼𝙪𝙙𝙞𝙤 / 𝙑𝙞𝙙𝙚𝙤 𝙏𝙚𝙡𝙚𝙜𝙧𝙖𝙢 𝙋𝙖𝙙𝙖 𝙎𝙚𝙢𝙗𝙖𝙣𝙜 𝙎𝙪𝙖𝙧𝙖 𝙆𝙪𝙢𝙥𝙪𝙡𝙖𝙣 𝙏𝙚𝙡𝙚𝙜𝙧𝙖𝙢. 𝙈𝙖𝙧𝙞 𝙉𝙞𝙠𝙢𝙖𝙩𝙞 𝙋𝙖𝙣𝙙𝙖𝙣𝙜𝙖𝙣 𝙎𝙞𝙣𝙚𝙢𝙖𝙩𝙞𝙠 𝙋𝙚𝙢𝙖𝙞𝙣 𝙑𝙞𝙙𝙚𝙤 𝙆𝙪𝙢𝙥𝙪𝙡𝙖𝙣 𝘽𝙚𝙧𝙨𝙖𝙢𝙖 𝙍𝙖𝙠𝙖𝙣 𝘼𝙣𝙙𝙖🥰! \n\n**𝙈𝙖𝙙𝙚 𝙒𝙞𝙩𝙝 ❤️ 𝘽𝙮 @SanzuCodingMY!** 👑"
 HELP_TEXT = f"""
 🛠-- **Setting Up Bot**:--
 
